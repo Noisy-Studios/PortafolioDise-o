3bfe209b15216bacea6cfb241cef005ac958c7ff
Juan David Ríos Sánchez <juandavidriossanchez11@gmail.com>
2026-08-26 12:26:19 -0500

    huh

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,17 +5,6 @@ function Home() {
   return (
     <>
       <h1>Inicio</h1>
-      <div className="home-buttons">
-        <Link to="/registro" className="btn">
-          Registro
-        </Link>
-        <Link to="/galeria" className="btn">
-          Galeria
-        </Link>
-        <Link to="/creditos" className="btn">
-          Creditos
-        </Link>
-      </div>
     </>
   );
 }
