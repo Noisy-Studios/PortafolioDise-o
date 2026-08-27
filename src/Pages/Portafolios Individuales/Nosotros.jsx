93020ddf300fec3caf14859927a39cb46624c367
estefania <estefaao2006@gmail.com>
2026-08-27 14:37:58 -0500

    Revert "pagina estefa"
    This reverts commit 688e70467b9d135bb0e115e40405005b55b7bc32.

--- src/Pages/Portafolios Individuales/Nosotros.jsx
+++ src/Pages/Portafolios Individuales/Nosotros.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router";
 import "./Portafolios.css";
 import Rios from "./Rios/Rios";
 import David from "./David/David";
-import Estefa from "./Estefania/Estefa";
 import MenuPortafolios from "../../components/ui/menu/PortafolioMenus";
 
 function Nosotros() {
@@ -12,7 +11,6 @@ function Nosotros() {
       <Routes>
         <Route path="rios" element={<Rios />} />
         <Route path="david" element={<David />} />
-        <Route path="estefania" element={<Estefa />} />
 
         {/* Agrega aquí las demás pestañas/rutas */}
         {/* <Route path="otra-pestana" element={<OtraPestana />} /> */}
