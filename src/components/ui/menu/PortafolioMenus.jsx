5bb39a2c04c7254141cacd54edef94165f0e1a83
davi0309 <david.saa0309@gmail.com>
2026-08-27 14:18:33 -0500

    se agrega la pestaña de david al portafolio

--- src/components/ui/menu/PortafolioMenus.jsx
+++ src/components/ui/menu/PortafolioMenus.jsx
@@ -6,7 +6,7 @@ function MenuPortafolios() {
     <nav className="menu-container">
       {/* Usar la ruta completa /Portafolios/... evita que se acumulen en la URL */}
       <Link to="/Nosotros/rios" className="menu-item">Rios</Link>
-      
+      <Link to="/Nosotros/david" className="menu-item">David</Link>
     </nav>
   );
 }
