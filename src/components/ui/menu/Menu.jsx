e23df1230e8956dfd9d57958a4f7c496fc826d5a
Juan David Ríos Sánchez <70483883+Juandavid1612@users.noreply.github.com>
2026-08-29 14:17:26 -0500

    jm

--- src/components/ui/menu/Menu.jsx
+++ src/components/ui/menu/Menu.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router";
+import "./Menu.css"; // si tienes uno
+
 function Menu() {
   return (
     <nav>
-      <a href={import.meta.env.BASE_URL}>Inicio</a>
-      <a href={`${import.meta.env.BASE_URL}nosotros`}>Nosotros</a>&nbsp;
-      <a href={`${import.meta.env.BASE_URL}juegos`}>Juegos</a>&nbsp;
-      <a href={`${import.meta.env.BASE_URL}contactanos`}>Contactanos</a>
+      <Link to="/">Inicio</Link>
+      <Link to="/Nosotros">Nosotros</Link>&nbsp;
+      <Link to="/juegos">Juegos</Link>&nbsp;
+      <Link to="/contactanos">Contactanos</Link>
     </nav>
   );
 }
--- src/components/ui/menu/PortafolioMenus.jsx
+++ src/components/ui/menu/PortafolioMenus.jsx
@@ -4,25 +4,9 @@ import "./PortafolioMenus.css";
 function MenuPortafolios() {
   return (
     <nav className="menu-container">
-      <Link
-        to={`${import.meta.env.BASE_URL}Nosotros/rios`}
-        className="menu-item"
-      >
-        Rios{" "}
-      </Link>
-      ```
-      <Link
-        to={`${import.meta.env.BASE_URL}Nosotros/david`}
-        className="menu-item"
-      >
-        David
-      </Link>
-      <Link
-        to={`${import.meta.env.BASE_URL}Nosotros/estefania`}
-        className="menu-item"
-      >
-        Estefania
-      </Link>
+      <Link to="/Nosotros/rios" className="menu-item">Rios</Link>
+      <Link to="/Nosotros/david" className="menu-item">David</Link>
+      <Link to="/Nosotros/estefania" className="menu-item">Estefania</Link>
     </nav>
   );
 }
